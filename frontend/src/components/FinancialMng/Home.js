395890450ecd8f1e46ccd082c21ec39be27026b7
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2024-08-27 16:45:15 +0530

    first pull request Sasmitha

---
+++ frontend/src/components/FinancialMng/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>- available transaction accouts
+                <br/>- notifications
+                <br/>- customer care
+                <br/>- Tutorials </p>
+        </div>
+    );
+}
+
+export default Home;
